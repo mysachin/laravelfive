21bfccfc8c421ca6779e2adcbdf9a3fffed6752e
SACHIN THAKUR <themysachin@gmail.com>
2020-04-19 17:32:41 +0530

    migration completed

---
+++ database/migrations/2020_04_19_115459_create_comment_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCommentTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comments', function (Blueprint $table) {
+            $table->string('commentable_type');
+            $table->integer('commentable_id');
+            $table->string('comment');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('comments');
+    }
+}
